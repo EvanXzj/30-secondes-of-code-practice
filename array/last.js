4107beb77c8c9d8ee57c28e6ba5e26f7cc8d16ad
EvanXzj <2686465170@qq.com>
2018-03-19 21:43:37 +0800

    write more

---
+++ array/last.js
@@ -0,0 +1,5 @@
+const last = (arr) => arr.slice(-1)
+
+// console.log(last([1, 2, 3]))
+
+exports.last = last
---
+++ array/tail.js
@@ -0,0 +1,3 @@
+const tail = (arr) => (arr.length > 1 ? arr.slice(1) : arr)
+
+exports.tail = tail
